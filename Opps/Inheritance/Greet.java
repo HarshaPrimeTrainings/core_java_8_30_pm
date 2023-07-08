145548968be5510adbe3ae3e098ee432236c5f21
HarshaVardhan <athaluri.harsha@gmail.com>
2023-07-08 21:19:44 +0530

    inheritance

---
+++ Opps/Inheritance/Greet.java
@@ -0,0 +1,11 @@
+class Greet{
+	
+	String msg = "Secret";
+	
+	public void sayHello(String name){
+		System.out.println("Hello "+ name);
+
+	}
+
+	
+}
---
+++ Opps/Inheritance/mainapp.java
@@ -0,0 +1,13 @@
+
+class MainApp extends Greet{
+	
+		
+	public static void main(String[] args){
+		
+		MainApp m =  new MainApp();
+		m.sayHello("Anand");
+		System.out.println(m.msg);
+		
+	}
+	
+}
