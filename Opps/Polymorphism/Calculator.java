96b410981eae4c40fc6267beec59662ba674a766
HarshaVardhan <athaluri.harsha@gmail.com>
2023-07-12 21:20:11 +0530

    polymorphism

---
+++ Opps/Polymorphism/Calculator.java
@@ -0,0 +1,16 @@
+class Calculator{
+	
+	void add(int a, int b){
+		System.out.println(a+b);
+	}
+	
+
+	
+	void add(int a, int b,int c){
+		System.out.println(a+b+c);
+	}
+	
+	void add(int a, int b,int c,int d){
+		System.out.println(a+b+c+d);
+	}
+}
---
+++ Opps/Polymorphism/MainApp.java
@@ -0,0 +1,15 @@
+class MainApp{
+	
+	
+	public static void main(String[] args){
+		Calculator calc = new Calculator();
+		
+		calc.add(2,5);
+		calc.add(2,5,3);
+		calc.add(2,5,3,4);
+		
+		
+		
+	}
+	
+}
